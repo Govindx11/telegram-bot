0fb9b001b226d1ee5cf1e832bf99429b368661e3
Govindxk11 <govindxk11@gmail.com>
2026-03-21 22:30:08 +0530

    Use environment variable for bot token
    Replaced hardcoded bot token with environment variable.

--- bot.py
+++ bot.py
@@ -4,7 +4,8 @@ import shutil
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes, CommandHandler
 
-BOT_TOKEN = "8232869946:AAEaNlvo57AXOo9dZDRE9JMIt82cBpWzmIs"
+# ✅ FIXED TOKEN
+BOT_TOKEN = os.getenv("8232869946:AAEaNlvo57AXOo9dZDRE9JMIt82cBpWzmIs")
 
 L = instaloader.Instaloader(download_videos=True, save_metadata=False)
 
